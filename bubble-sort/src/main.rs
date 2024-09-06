2193c4c8b3bb621863729cd50a34c8ff9b4eedf4
Ismail Karabulut <i.karabulut93@gmail.com>
2024-09-05 21:22:39 -0500

    add ability to dynamically render an array of random numbers

--- bubble-sort/src/main.rs
+++ bubble-sort/src/main.rs
@@ -1,5 +1,8 @@
+mod pseudo_rand_gen;
+
 use std::io;
 use std::io::Write;
+use crate::pseudo_rand_gen::PseudoRandGen;
 
 fn get_i32(prompt: &str) -> i32 {
     println!("{prompt}");
@@ -15,9 +18,22 @@ fn get_i32(prompt: &str) -> i32 {
         .expect("Error parsing integer")
 }
 
+fn make_random_vec(max_size: i32, max: i32) -> Vec<i32> {
+    let mut prng = PseudoRandGen::new();
+
+    let mut vec: Vec<i32> = Vec::with_capacity(max_size as usize);
+    for _ in 0..max_size {
+        vec.push(prng.next_i32(0, max));
+    }
+    vec
+}
+
 fn main() {
     let length = get_i32("Please enter the number of items you want to sort:");
     let max_size = get_i32("Please enter the max length of the array to sort:");
 
-    println!("{} length, {} size", length, max_size)
+    let vec_to_sort = make_random_vec(length, max_size);
+
+    println!("List to sort::");
+    println!("{:#?}", vec_to_sort);
 }
---
+++ bubble-sort/src/pseudo_rand_gen.rs
@@ -0,0 +1,42 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct PseudoRandGen {
+    seed: u32,
+}
+
+impl PseudoRandGen {
+    pub fn new() -> Self {
+        let mut psuedo_rand_gen = Self { seed: 0 };
+        psuedo_rand_gen.randomize();
+        psuedo_rand_gen
+
+    }
+
+    pub fn randomize(&mut self) {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis();
+        self.seed = millis as u32
+    }
+
+    // Return a pseudorandom value in the range [0, 2147483647].
+    pub fn next_u32(&mut self) -> u32 {
+        self.seed = self.seed.wrapping_mul(1_103_515_245).wrapping_add(12_345);
+        self.seed %= 1 << 31;
+        self.seed
+    }
+
+    // Return a pseudorandom value in the range [0.0, 1.0).
+    pub fn next_f64(&mut self) -> f64 {
+        let f = self.next_u32() as f64;
+        f / (2147483647.0 + 1.0)
+    }
+
+    // Return a pseudorandom value in the range [min, max).
+    pub fn next_i32(&mut self, min: i32, max: i32) -> i32 {
+        let range = (max - min) as f64;
+        let result = min as f64 + range * self.next_f64();
+        result as i32
+    }
+}
